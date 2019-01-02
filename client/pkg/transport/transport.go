ad5d252fea02946da3a59fd6ec80de771ffe1888
Alan Potter <alan.potter@cyberark.com>
2019-01-02 14:14:30 -0500

    Disable keep alives

--- client/pkg/transport/transport.go
+++ client/pkg/transport/transport.go
@@ -49,6 +49,7 @@ func NewTransport(info TLSInfo, dialtimeoutd time.Duration) (*http.Transport, er
 		// value taken from http.DefaultTransport
 		TLSHandshakeTimeout: 10 * time.Second,
 		TLSClientConfig:     cfg,
+		DisableKeepAlives:   true,
 	}
 
 	dialer := &net.Dialer{
@@ -64,6 +65,7 @@ func NewTransport(info TLSInfo, dialtimeoutd time.Duration) (*http.Transport, er
 		DialContext:         dialContext,
 		TLSHandshakeTimeout: 10 * time.Second,
 		TLSClientConfig:     cfg,
+		DisableKeepAlives:   true,
 		// Cost of reopening connection on sockets is low, and they are mostly used in testing.
 		// Long living unix-transport connections were leading to 'leak' test flakes.
 		// Alternatively the returned Transport (t) should override CloseIdleConnections to
